c1f17a2f27bf880876f29d067650c5ff95c3274c
madnik7 <madnik7@gmail.com>
2018-04-08 14:30:37 +0430

    remove test data

--- src/DirectSpAuth/AuthUtil.cs
+++ src/DirectSpAuth/AuthUtil.cs
@@ -1,9 +1,6 @@
 ﻿using AspNet.Security.OpenIdConnect.Primitives;
-using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace DirectSp.AuthServer
 {
